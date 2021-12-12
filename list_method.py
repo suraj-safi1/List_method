b3bba6fa2f9643ce90326b862d7ceb56b7967ae3
SURAJ SAFI <94732511+suraj-safi1@users.noreply.github.com>
2021-12-12 12:33:15 +0530

    Methods Of List

---
+++ list_method.py
@@ -0,0 +1,26 @@
+# List methods
+list=[12,45,67,3,5,89,90,45,33,67,78]
+print(list)
+list.sort()
+print("list after sort method",list)
+list.reverse()
+print("list after reverse method",list)
+list.append(90)
+print("list after append fenction",list)
+list.insert(4,66)
+print("list after insert function used",list)
+list.pop(5)
+print("list after pop method used",list)
+list.remove(90)
+print("list after remove function",list)
+print(list)
+list.pop()
+print("after pop user",list)
+del list[4]
+print("list after del keyword",list)
+del list
+print("list after del ",list)
+list=[12,45,67,3,5,89,90,45,33,67,78]
+print(list)
+list.clear()
+print("list after clear function",list)
